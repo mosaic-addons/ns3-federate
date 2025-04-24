c489ce51669a7c0bdfc07cd5602179741ea59b07
Georg Hoelger <georg.hoelger@fokus.fraunhofer.de>
2025-04-24 13:00:48 +0200

    Always deploy files at ns3/build/lib folder

--- run_from_ns3installer.sh
+++ run_from_ns3installer.sh
@@ -9,6 +9,6 @@ fi
 
 ns3Version="3.36.1"
 
-LD_LIBRARY_PATH=./ns-allinone-$ns3Version/ns-$ns3Version/build\
+LD_LIBRARY_PATH=./ns-allinone-$ns3Version/ns-$ns3Version/build/lib\
     ./ns3-federate\
     --port=$port --cmdPort=$cmdport --configFile=ns3config/ns3_federate_config.xml
