d208ea36094535dc782c97bb97564777493ae1be
Georg Hoelger <georg.hoelger@fokus.fraunhofer.de>
2025-04-24 14:04:06 +0200

    Add run file for manual development

---
+++ run_manually.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+port=$1
+cmdport=$2
+
+if [[ -z $port ]]; then
+    port=5011
+fi
+
+if [[ -z $cmdport ]]; then
+    cmdport=0
+fi
+
+ns3Version="3.36.1"
+
+LD_LIBRARY_PATH=../ns-allinone-$ns3Version/ns-$ns3Version/build/lib\
+    ./bin/Debug/ns3-federate\
+    --port=$port --cmdPort=$cmdport --configFile=ns3_federate_config.xml
+    
