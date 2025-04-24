34e2b56e509ce9528dbb31030203d5581fc85a08
Georg Hoelger <georg.hoelger@fokus.fraunhofer.de>
2025-04-24 10:41:16 +0200

    Do not rename to 'mosaic_starter'

--- run_from_ns3installer.sh
+++ run_from_ns3installer.sh
@@ -11,5 +11,5 @@ ns3Version="3.36.1"
 
 cd ns3
 LD_LIBRARY_PATH=../ns-allinone-$ns3Version/ns-$ns3Version/build\
-    ../ns-allinone-$ns3Version/ns-$ns3Version/build/scratch/mosaic_starter\
+    ../ns-allinone-$ns3Version/ns-$ns3Version/build/scratch/ns3-federate\
     --port=$port --cmdPort=$cmdport --configFile=scratch/ns3_federate_config.xml
