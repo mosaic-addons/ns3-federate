b5ef46052c9648fc930ee926e29268d58f39c6bd
Georg Hoelger <georg.hoelger@fokus.fraunhofer.de>
2025-04-24 10:37:11 +0200

    refactor indentation

--- run_from_ns3installer.sh
+++ run_from_ns3installer.sh
@@ -4,10 +4,12 @@ port=$1
 cmdport=$2
 
 if [[ -z $cmdport ]]; then
-  cmdport=0
+    cmdport=0
 fi
 
 ns3Version="3.36.1"
 
 cd ns3
-LD_LIBRARY_PATH=../ns-allinone-$ns3Version/ns-$ns3Version/build ../ns-allinone-$ns3Version/ns-$ns3Version/build/scratch/mosaic_starter --port=$port --cmdPort=$cmdport --configFile=scratch/ns3_federate_config.xml
+LD_LIBRARY_PATH=../ns-allinone-$ns3Version/ns-$ns3Version/build\
+    ../ns-allinone-$ns3Version/ns-$ns3Version/build/scratch/mosaic_starter\
+    --port=$port --cmdPort=$cmdport --configFile=scratch/ns3_federate_config.xml
